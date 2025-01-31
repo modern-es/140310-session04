9a798611265bc325fae6c1517cf9ff94d7e56ecb
Pashaie <pashaie@gmail.com>
2025-01-31 11:24:29 +0330

    template engine

---
+++ s04/src/main.js
@@ -0,0 +1,21 @@
+import Handlebars from "handlebars";
+const studentTmpl = document.querySelector('#studentTmpl')
+const students = [
+    { name: 'Alireza Abolhasani' },
+    { name: 'Zahra Aghaei' },
+    { name: 'Hadi Behdad' },
+    { name: 'Mohammad Farahypour' },
+    { name: 'Setare Modaresi' },
+    { name: 'Atefeh Daneshvar' },
+    { name: 'Sara Farani Sani' },
+    { name: 'Nastaran Malekpoor' },
+    { name: 'Seyedeh Zahra Ashrafian' },
+]
+
+//section 2
+const template = Handlebars.compile(studentTmpl.innerHTML);
+const html = template({ data: students });
+
+const app = document.querySelector('#app')
+app.innerHTML = html;
+console.log(html)
